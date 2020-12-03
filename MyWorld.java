fc96b5f9d22dc4bf16cc88e9df716d14befcc9b9
Reevak05 <kaveergera@gmail.com>
2020-12-03 10:09:21 -0800

    first commit and set world size

---
+++ MyWorld.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
+        // Create a new world with 400x400 cells with a cell size of 100x100 pixels.
+        super(4, 4, 100); 
+    }
+}
